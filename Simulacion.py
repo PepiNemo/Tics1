ade8f593242c4f96d8c2bb614b7ccc047c46548c
FelipeUlloa <felipe.ulloa1@mail.udp.cl>
2020-08-24 22:05:14 -0400

    PrimerEsquemaCorrecion

--- Simulacion.py
+++ Simulacion.py
@@ -1,96 +1,92 @@
 import Caja
 import Cliente
 
+
 def CajaMenorCola(Caja):
     return 3
 
 
-def TemporizadorClientes( ClientesSeleccionando, Cajas, IndiceCajaMasVacia):
-    #Escribir Logica
-    #Recorrer ClientesSeleccionando y quitarle 1 segundos a su temporizador
-    #Si el temporizador llega a 0 Insertar en la caja Mas Vacia
-    
-    for i in range (len(ClientesSeleccionando)):
+def TemporizadorClientes(ClientesSeleccionando, Cajas, IndiceCajaMasVacia):
+    # Escribir Logica
+    # Recorrer ClientesSeleccionando y quitarle 1 segundos a su temporizador
+    # Si el temporizador llega a 0 Insertar en la caja Mas Vacia
+
+    for i in range(len(ClientesSeleccionando)):
         if(ClientesSeleccionando[i].Temporizador <= 0):
             Cajas[IndiceCajaMasVacia].AgregarCliente(ClientesSeleccionando[i])
-            IndiceCajaMasVacia=CajaMenorCola(Cajas)
-            #Si la caja no estaba atendiendo
+            IndiceCajaMasVacia = CajaMenorCola(Cajas)
+            # Si la caja no estaba atendiendo
             if(Cajas[IndiceCajaMasVacia].Temporizador == -1):
-                #Que comience a atender inicializando el Temporizador
+                # Que comience a atender inicializando el Temporizador
                 Cajas[IndiceCajaMasVacia].AtenderSiguienteCliente()
-            
-        #Restamos los temporizadores
-        ClientesSeleccionando[i].Temporizador=ClientesSeleccionando[i].Temporizador-1
+
+        # Restamos los temporizadores
+        ClientesSeleccionando[i].Temporizador = ClientesSeleccionando[i].Temporizador-1
+
 
 def TemporizadorCajas(Cajas):
     for i in range(len(Cajas)):
+
         if(Cajas[i].Temporizador > 0):
-            Cajas[i].Temporizador=Cajas[i].Temporizador -1
-        elif (Cajas[i].Temporizador==0):
+            Cajas[i].Temporizador = Cajas[i].Temporizador-1
+
+        elif(Cajas[i].Temporizador == 0):
+            # Si no tiene mas Clientes Deja de Atender
+            if not Cajas[i].Cola:
+                Cajas[i].Temporizador = -1
+
+            # Si tiene mas clientes en cola, que Atienda al Siguiente
             Cajas[i].AtenderSiguienteCliente()
-        
 
 
-#Datos Ingresados por el Cliente
-NumeroTotalClientes=1200
+# Datos Ingresados por el Cliente
+NumeroTotalClientes = 1200
 
-#Arreglos de 10 elementos para cada Intervalo
-CajasAbiertas=15
-DistribucionPorcentual=1200*0.33
-#Datos que iran en los atributos de las Clases
-#TP=TiempoPromedio
-TPSeleccionProducto=30
-TPMarcadoCajaProducto=10
-TPPagoCliente=45
+# Arreglos de 10 elementos para cada Intervalo
+CajasAbiertas = 15
+DistribucionPorcentual = 1200*0.33
+# Datos que iran en los atributos de las Clases
+# TP=TiempoPromedio
+TPSeleccionProducto = 30
+TPMarcadoCajaProducto = 10
+TPPagoCliente = 45
 
 
-#################Simulacion#####################
+####################################  Simulacion  ############################
 
-#Genarar Las Cajas de Un intervalo
-Cajas=[]
+# Genarar Las Cajas de Un intervalo
+Cajas = []
 for i in range(CajasAbiertas):
-    NuevaCaja=Caja.Caja(TPMarcadoCajaProducto,TPPagoCliente)
+    NuevaCaja = Caja.Caja(TPMarcadoCajaProducto, TPPagoCliente)
     Cajas.append(NuevaCaja)
 
-#Entra el primer Cliente
-ClientesSeleccionando=[]
-NuevoCliente=Cliente.Cliente(TPSeleccionProducto)
-ClientesSeleccionando.append(NuevoCliente)
+#Almacen de clientes Seleccionando Productos
+ClientesSeleccionando = []
 
-#Cada cuanto (s)entrara el siguiente cliente ("Los clientes se distribuyen de forma uniforme en el tiempo")
-TPEntraCliente=3600 // DistribucionPorcentual
 
-#Empieza el tiempo de un Intervalo
-for j in range(3600):#1 Hora Osea un intervalo
-    #Corremos 1 segundo en los temporizadores
+# Cada cuanto (s)entrara el siguiente cliente ("Los clientes se distribuyen de forma uniforme en el tiempo")
+TPEntraCliente = 3600 // DistribucionPorcentual
+#Ha que caja ira el proximo Cliente
+IndiceCajaMasVacia = CajaMenorCola(Cajas)
 
-    #Vista Clientes
-    TPEntraCliente=TPEntraCliente-1
-    if(TPEntraCliente <= 0): #Entra un NuevoCliente 
-        NuevoCliente=Cliente.Cliente(TPSeleccionProducto)
+# Empieza el tiempo de un Intervalo
+for j in range(3600):  
+    # Corremos 1 segundo en los temporizadores
+
+    # Vista Clientes
+
+    #Entradas de los Clientes
+    TPEntraCliente = TPEntraCliente-1
+    if(TPEntraCliente == 0 or j==0):  # Entra un NuevoCliente
+        NuevoCliente = Cliente.Cliente(TPSeleccionProducto)
         ClientesSeleccionando.append(NuevoCliente)
-        TPEntraCliente=3600/DistribucionPorcentual
-    #Quitar 1 segundos a los clientes seleccionando, para dps ir a Caja
-    IndiceCajaMasVacia= CajaMenorCola(Cajas)
-    TemporizadorClientes(ClientesSeleccionando,Cajas, IndiceCajaMasVacia)
+        TPEntraCliente = 3600/DistribucionPorcentual
 
-    #Vista Caja
-    #Quitar 1 segundo a los clientes Seleccionando, para dps despachar
+    # Quitar 1 segundos a los clientes seleccionando, para dps ir a Caja
+    TemporizadorClientes(ClientesSeleccionando, Cajas, IndiceCajaMasVacia)
+
+    # Vista Caja
+    # Quitar 1 segundo a los clientes Seleccionando, para dps despachar
     TemporizadorCajas(Cajas)
-
-    for i in range(len(Cajas)):
-        if(Cajas[i].Temporizador > -1):
-            Cajas[i].Temporizador=Cajas[i].Temporizador-1
-
-            if(Cajas[i].Temporizador==0):
-                #Si no tiene mas Clientes Deja de Atender
-                if not Cajas[i].Cola:
-                    Cajas[i].Temporizador=-1
-                
-                #Si tiene mas clientes en cola, que Atienda al Siguiente
-                Cajas[i].AtenderSiguienteCliente()
-                
-
-
 
 
