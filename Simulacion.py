6aa3a7a4330f41d7f4956bcbda44e3230c83e9cc
FelipeUlloa <felipe.ulloa1@mail.udp.cl>
2020-08-25 21:37:39 -0400

    presentar

--- Simulacion.py
+++ Simulacion.py
@@ -3,10 +3,10 @@ import Cliente
 
 
 def CajaMenorCola(Cajas):
+    # Retorna el Indice de la Caja con la Fila mas Corta
     return 3
 
-
-def TemporizadorClientes(ClientesSeleccionando, Cajas, IndiceCajaMasVacia):
+def TemporizadorClientes(ClientesSeleccionando, Cajas, IndiceCajaMasVacia, InterValo):
     # Escribir Logica
     # Recorrer ClientesSeleccionando y quitarle 1 segundos a su temporizador
     # Si el temporizador llega a 0 Insertar en la caja Mas Vacia
@@ -23,6 +23,8 @@ def TemporizadorClientes(ClientesSeleccionando, Cajas, IndiceCajaMasVacia):
         # Restamos los temporizadores
         ClientesSeleccionando[i].Temporizador = ClientesSeleccionando[i].Temporizador-1
 
+def InsertarClienteCaja(IndiceClienteSelecccionando, Cajas, IndiceCajaMasVacia):
+    a=1
 
 def TemporizadorCajas(Cajas):
     for i in range(len(Cajas)):
@@ -43,8 +45,9 @@ def TemporizadorCajas(Cajas):
 NumeroTotalClientes = 1200
 
 # Arreglos de 10 elementos para cada Intervalo
-CajasAbiertas = 15
-DistribucionPorcentual = 1200*0.33
+CajasAbiertas = [5, 5, 5, 10, 10, 10, 15, 15, 10, 10]
+DistribucionPorcentual = [0.05, 0.07, 0.09,
+                          0.11, 0.14, 0.14, 0.09, 0.07, 0.11, 0.13]
 # Datos que iran en los atributos de las Clases
 # TP=TiempoPromedio
 TPSeleccionProducto = 30
@@ -56,37 +59,35 @@ TPPagoCliente = 45
 
 # Genarar Las Cajas de Un intervalo
 Cajas = []
-for i in range(CajasAbiertas):
-    NuevaCaja = Caja.Caja(TPMarcadoCajaProducto, TPPagoCliente)
-    Cajas.append(NuevaCaja)
-
-#Almacen de clientes Seleccionando Productos
+# Almacen de clientes Seleccionando Productos
 ClientesSeleccionando = []
+for i in range(10):#i =Intervalos
+    for j in range(CajasAbiertas[i]):
+        NuevaCaja = Caja.Caja(TPMarcadoCajaProducto, TPPagoCliente, i)
+        Cajas.append(NuevaCaja)
 
 
-# Cada cuanto (s)entrara el siguiente cliente ("Los clientes se distribuyen de forma uniforme en el tiempo")
-TPEntraCliente = 3600 // DistribucionPorcentual
-#Ha que caja ira el proximo Cliente
-IndiceCajaMasVacia = CajaMenorCola(Cajas)
+    # Cada cuanto (s)entrara el siguiente cliente ("Los clientes se distribuyen de forma uniforme en el tiempo")
+    TPEntraCliente = 3600 // (NumeroTotalClientes*DistribucionPorcentual[i])
+    # Ha que caja ira el proximo Cliente
+    IndiceCajaMasVacia = CajaMenorCola(Cajas)
 
-# Empieza el tiempo de un Intervalo
-for j in range(3600):  
-    # Corremos 1 segundo en los temporizadores
+    # Empieza el tiempo de un solo Intervalo
+    for z in range(3600):
+        # Corremos 1 segundo en los temporizadores
 
-    # Vista Clientes
+        # Vista Clientes
 
-    #Entradas de los Clientes
-    TPEntraCliente = TPEntraCliente-1
-    if(TPEntraCliente == 0 or j==0):  # Entra un NuevoCliente
-        NuevoCliente = Cliente.Cliente(TPSeleccionProducto)
-        ClientesSeleccionando.append(NuevoCliente)
-        TPEntraCliente = 3600/DistribucionPorcentual
+        # Entradas de los Clientes
+        TPEntraCliente = TPEntraCliente-1
+        if(TPEntraCliente == 0 or z == 0):  # Entra un NuevoCliente
+            NuevoCliente = Cliente.Cliente(TPSeleccionProducto)
+            ClientesSeleccionando.append(NuevoCliente)
+            TPEntraCliente = 3600 // (NumeroTotalClientes*DistribucionPorcentual[i])
 
-    # Quitar 1 segundos a los clientes seleccionando, para dps ir a Caja
-    TemporizadorClientes(ClientesSeleccionando, Cajas, IndiceCajaMasVacia)
+        # Quitar 1 segundos a los clientes seleccionando, para dps ir a Caja (Temporizador e InsertarCliente)
+        TemporizadorClientes(ClientesSeleccionando, Cajas, IndiceCajaMasVacia, i)
 
-    # Vista Caja
-    # Quitar 1 segundo a los clientes Seleccionando, para dps despachar
-    TemporizadorCajas(Cajas)
-
-
+        # Vista Caja
+        # Quitar 1 segundo a los clientes Seleccionando, para dps despachar
+        TemporizadorCajas(Cajas)
