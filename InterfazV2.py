655cb12d6b607b67e02bfe3e50d3926070ba5eaf
mringeling1 <matias.ringeling@mail.udp.cl>
2020-08-29 18:47:29 -0400

    Todo posicionado, falta lo que esta comentado

--- InterfazV2.py
+++ InterfazV2.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import *
 
-#cambiar .pack() por .place(x=,y=)
+#Arreglar Boton
 class Simulador():
     def __init__(self, master):
         self.master = master
@@ -9,82 +9,86 @@ class Simulador():
 
         '''----------Descripcion----------'''
         self.lblDesc = Label(text="Descripcion")
-        self.lblDesc.pack()
-        self.desc = Entry(width=60)
+        self.lblDesc.place(x=20, y = 10)
+        self.desc = Entry()
+        self.desc.insert(0,"Ingresa descipcion") #if error wea.insert(valor agregado antes)
         self.desc.focus()
-        self.desc.pack()
+        self.desc.place(x=130, y=10, width = 100)
 
         '''----------Horas de atencion----------'''
         self.lblHoras = Label(text="Horas de Atencion")
-        self.lblHoras.pack()
+        self.lblHoras.place(x=20, y = 40)
         self.horas = Entry(width=60)
-        self.horas.pack()
+        self.horas.place(x=130, y = 40, width = 100)
 
         '''----------Periodos de Tiempo----------'''
         self.lblPeriodos = Label(text="Periodos de tiempo")
-        self.lblPeriodos.pack()
+        self.lblPeriodos.place(x=20, y = 70)
         self.periodos = Entry(width=60)
-        self.periodos.pack()
+        self.periodos.place(x = 130, y = 70, width = 100)
 
         '''----------CLientes Esperados----------'''
         self.lblClientes = Label(text="Clientes Esperados")
-        self.lblClientes.pack()
+        self.lblClientes.place(x=20, y = 100)
         self.clientes = Entry(width=60)
-        self.clientes.pack()
+        self.clientes.place(x = 130, y = 100, width = 100)
 
         '''----------Distribucion----------'''
-        self.lblDistribucion = Label(text="Distribucion clietnes por periodo")
-        self.lblDistribucion.pack()
+        self.lblDistribucion = Label(text="Distribucion")
+        self.lblDistribucion.place(x=20, y = 130)
         self.distribucion = Entry(width=60)
-        self.distribucion.pack()
+        self.distribucion.place(x=130, y = 130, width = 100)
 
         '''----------Cajas Abiertas----------'''
-        self.lblCajas = Label(text="Cajas Abiertas por Periodo")
-        self.lblCajas.pack()
+        self.lblCajas = Label(text="Cajas Abiertas")
+        self.lblCajas.place(x=20, y = 160)
         self.cajas = Entry(width=60)
-        self.cajas.pack()
+        self.cajas.place(x=130, y = 160, width = 100)
 
         '''----------Cantidad Minima de Productos----------'''
-        self.lblCMinProductos = Label(text="Cantidad Minima de Productos")
-        self.lblCMinProductos.pack()
+        self.lblCMinProductos = Label(text="Productos minimos")
+        self.lblCMinProductos.place(x=20, y = 130)
         self.minprod = Entry(width=60)
-        self.minprod.pack()
+        self.minprod.place(x=130, y = 130, width = 100)
 
         '''----------Cantidad Maxima de Productos----------'''
-        self.lblCMaxProductos = Label(text="Cantidad Maxima de Productos")
-        self.lblCMaxProductos.pack()
+        self.lblCMaxProductos = Label(text="Productos Maximo")
+        self.lblCMaxProductos.place(x=20, y = 160)
         self.maxprod = Entry(width=60)
-        self.maxprod.pack()
+        self.maxprod.place(x=130, y = 160, width = 100)
 
         '''----------Tiempo Seleccion----------'''
-        self.lblTS = Label(text="Tiempo seleccion productos")
-        self.lblTS.pack()
+        self.lblTS = Label(text="Tiempo seleccion")
+        self.lblTS.place(x=20, y = 190)
         self.TS = Entry(width=60)
-        self.TS.pack()
+        self.TS.place(x=130, y = 190, width = 100)
 
         '''----------Tiempo Marcadp----------'''
         self.lblTM = Label(text="Tiempo Marcado")
-        self.lblTM.pack()
+        self.lblTM.place(x=20, y = 220)
         self.TM = Entry(width=60)
-        self.TM.pack()
+        self.TM.place(x=130, y = 220, width = 100)
 
         '''----------Tiempo Pago----------'''
         self.lblTP = Label(text="Tiempo Pago")
-        self.lblTP.pack()
+        self.lblTP.place(x=10, y =250, width = 100)
         self.TP = Entry(width=60)
-        self.TP.pack()
+        self.TP.place(x=130, y = 250, width = 100)
 
         self.button = tk.Button(self.frame, text="Simular", command=self.simulate)
         self.button.pack()
+        self.button.place(x=130,y=280)
+        #arreglar boton que no aparece
         self.frame.pack()
 
         '''-----------METODOS-----------'''
 
     def simulate(self):
-        try:
+        try: #Se puede hacer sin exceptions
             Desc = self.desc.get()
             BoolDesc = True
         finally:
+            Desc = self.desc.get()
             if len(Desc) <= 0 or len(Desc) > 60:
                 BoolDesc = False
 
@@ -107,30 +111,35 @@ class Simulador():
         except:
             print("Valor ingresado en cantidad de clientes no entero")
             BoolClientes = False
+
         try:
             Minprod = int(self.minprod.get())
             BoolMinProd = True
         except:
             print("Valor ingresado en minimo productos no entero")
             BoolMinProd = False
+
         try:
             Maxprod = int(self.maxprod.get())
             BoolMaxProd = True
         except:
             print("Valor ingresado en maximo Productos no entero")
             BoolMaxProd = False
+
         try:
             Tsel = int(self.TS.get())
             BoolTsel = True
         except:
             print("Valor ingresado en tiempo de seleccion no entero")
             BoolTsel = False
+
         try:
             Tmar = int(self.TM.get())
             BoolTmar = True
         except:
             print("Valor ingresado en tiempo de marcado no entero")
             BoolTmar = False
+
         try:
             Tpago = int(self.TP.get())
             BoolTpago = True
@@ -155,6 +164,7 @@ class Simulador():
             print("Error ingresando datos en distribuicon porcentual")
             BoolDist = False
         #finally todos valores mayores que 0 y suma  = 100
+
         try: #excepcion todos los valores mayores que 0
             caj = self.cajas.get()
             cajArr = caj.split(",")
@@ -163,27 +173,22 @@ class Simulador():
                 j = int(i)
                 CAJAS.append(j)
             BoolCajas = True
-
         except:
             print("Error ingresando datos en cantidad de cajas")
             BoolCajas = False
         #finally todos os valores mayores que 0
 
-
         #if todas las condiciones True:
             #simulador(Datos)
+            #self.master.destroy()
         #else:
             #Abrir segunda pantalla indicando los datos mal ingresados
-            #Intentar setear por default los datos ingresados la anteriormente con Set
-        self.master.destroy()
-        print("simulando")
-
-
+            #Intentar setear por default los datos ingresados la anteriormente con .insert
 
 def main():
     root = Tk()
     root.title = "Simulador"
-    root.geometry('470x500')
+    root.geometry('470x550')
     app = Simulador(root)
     root.mainloop()
 
