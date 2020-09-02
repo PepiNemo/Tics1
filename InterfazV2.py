8a3e5bf1380f1129f96806d23467939f76c5a7de
mringeling1 <matias.ringeling@mail.udp.cl>
2020-09-01 21:32:43 -0400

    Casi listo

--- InterfazV2.py
+++ InterfazV2.py
@@ -1,93 +1,128 @@
 import tkinter as tk
 from tkinter import *
 
+def retornar():
+    desc = "Ingrese una descripcion"
+    horas = "0"
+    periodos = "0"
+    clientes = "0"
+    distr = "0"
+    cajas = "0"
+    min = "0"
+    max = "0"
+    ts = "0"
+    tm = "0"
+    tp = "0"
 
-def Simulacion(Desc, Horas, Periodos, Clientes, Minprod, Maxprod, Tsel, Tmar, Tpago, Distrib, CAJAS):
-    pass
+    root = Tk()
+    root.title = "Simulador"
+    root.geometry('300x385')
+    app = Simulador(root,desc,horas,periodos,clientes,distr,cajas,min,max,ts,tm,tp)
+    root.mainloop()
+    print(app.Horas)
+    lista = [Desc, Horas, Periodos, Clientes, Minprod, Maxprod, Tsel, Tmar, Tpago, Distrib, CAJAS]
+    return lista
 
-
-class Simulador():
-    def __init__(self, master):
+class Simulador:
+    def __init__(self, master,Desc,Horas,Periodos,Clientes,Distribucion,Cajas,Minprod,Maxprod,ts,tm,tp):
         self.master = master
         self.frame = tk.Frame(self.master)
-
+        self.Desc = Desc
+        self.Horas = Horas
+        self.Periodos = Periodos
+        self.Clientes = Clientes
+        self.Distrib = Distribucion
+        self.Cajas = Cajas
+        self.Min = Minprod
+        self.Max = Maxprod
+        self.ts = ts
+        self.tm = tm
+        self.tp = tp
         '''----------Descripcion----------'''
         self.lblDesc = Label(text="Descripcion")
         self.lblDesc.place(x=20, y=10)
         self.desc = Entry()
-        self.desc.insert(0, "Ingresa descipcion")  # if error wea.insert(valor agregado antes)
+        self.desc.insert(0,self.Desc)  # if error wea.insert(valor agregado antes)
         self.desc.focus()
         self.desc.place(x=130, y=10, width=130)
 
         '''----------Horas de atencion----------'''
-        self.lblHoras = Label(text="Horas de Atencion")
+        self.lblHoras = Label(text="Horas de Atencion") #rango de horas ej 08:00-18:00
         self.lblHoras.place(x=20, y=40)
         self.horas = Entry(width=60)
+        self.horas.insert(0,self.Horas)
         self.horas.place(x=130, y=40, width=130)
 
         '''----------Periodos de Tiempo----------'''
         self.lblPeriodos = Label(text="Periodos de tiempo")
         self.lblPeriodos.place(x=20, y=70)
         self.periodos = Entry(width=60)
+        self.periodos.insert(0,self.Periodos)
         self.periodos.place(x=130, y=70, width=130)
 
         '''----------CLientes Esperados----------'''
         self.lblClientes = Label(text="Clientes Esperados")
         self.lblClientes.place(x=20, y=100)
         self.clientes = Entry(width=60)
+        self.clientes.insert(0,self.Clientes)
         self.clientes.place(x=130, y=100, width=130)
 
         '''----------Distribucion----------'''
         self.lblDistribucion = Label(text="Distribucion")
         self.lblDistribucion.place(x=20, y=130)
         self.distribucion = Entry(width=60)
+        self.distribucion.insert(0,self.Distrib)
         self.distribucion.place(x=130, y=130, width=130)
 
         '''----------Cajas Abiertas----------'''
         self.lblCajas = Label(text="Cajas Abiertas")
         self.lblCajas.place(x=20, y=160)
         self.cajas = Entry(width=60)
+        self.cajas.insert(0,self.Cajas)
         self.cajas.place(x=130, y=160, width=130)
 
         '''----------Cantidad Minima de Productos----------'''
         self.lblCMinProductos = Label(text="Productos minimos")
         self.lblCMinProductos.place(x=20, y=190)
         self.minprod = Entry(width=60)
+        self.minprod.insert(0,self.Min)
         self.minprod.place(x=130, y=190, width=130)
 
         '''----------Cantidad Maxima de Productos----------'''
         self.lblCMaxProductos = Label(text="Productos Maximo")
         self.lblCMaxProductos.place(x=20, y=220)
         self.maxprod = Entry(width=60)
+        self.maxprod.insert(0,self.Max)
         self.maxprod.place(x=130, y=220, width=130)
 
         '''----------Tiempo Seleccion----------'''
         self.lblTS = Label(text="Tiempo seleccion")
         self.lblTS.place(x=20, y=250)
         self.TS = Entry(width=60)
+        self.TS.insert(0,self.ts)
         self.TS.place(x=130, y=250, width=130)
 
         '''----------Tiempo Marcadp----------'''
         self.lblTM = Label(text="Tiempo Marcado")
         self.lblTM.place(x=20, y=280)
         self.TM = Entry(width=60)
+        self.TM.insert(0,self.tm)
         self.TM.place(x=130, y=280, width=130)
 
         '''----------Tiempo Pago----------'''
         self.lblTP = Label(text="Tiempo Pago")
         self.lblTP.place(x=10, y=310, width=100)
         self.TP = Entry(width=60)
+        self.TP.insert(0,self.tp)
         self.TP.place(x=130, y=310, width=130)
 
         self.button = tk.Button(self.frame, text="Simular", command=self.simulate)
         self.button.pack()
-        # arreglar boton que no aparece
         self.frame.place(x=130, y=340)
 
         '''-----------METODOS-----------'''
-
     def simulate(self):
-        global Horas, Periodos, Clientes, Minprod, Maxprod, Tsel, Tmar, Distrib, Tpago, CAJAS
+        global Desc, Horas, Periodos, Clientes, Minprod, Maxprod, Tsel, Tmar, Distrib, Tpago, CAJAS
         BoolDesc = False
         BoolHoras = False
         BoolPeriodos = False
@@ -100,6 +135,7 @@ class Simulador():
         BoolDist = False
         BoolCajas = False
 
+        #finally if num <0 false
         try:  # Se puede hacer sin exceptions
             Desc = self.desc.get()
             BoolDesc = True
@@ -202,73 +238,68 @@ class Simulador():
                     BoolCajas = False
 
         if BoolDesc and BoolHoras and BoolPeriodos and BoolClientes and BoolMinProd and BoolMaxProd and BoolTsel and BoolTmar and BoolTpago and BoolDist and BoolCajas:
-            Simulacion(Desc, Horas, Periodos, Clientes, Minprod, Maxprod, Tsel, Tmar, Tpago, Distrib, CAJAS)
+            #Simulacion(Desc, Horas, Periodos, Clientes, Minprod, Maxprod, Tsel, Tmar, Tpago, Distrib, CAJAS)
             print("Simulando")
-
-            LabelSim = Label(text="Desea realizar una nueva simulacion?")
+            self.master.geometry('280x430')
+            LabelSim = Label(text="Simulacion en proceso.")
             LabelSim.place(x=10, y=370)
-            YesBtn = tk.Button(text="Si")
-            NoBtn = tk.Button(text="No", command=self.master.destroy)
-            YesBtn.place(x=10, y=390, width=30)  # Hacer Funcion Yes
-            NoBtn.place(x=40, y=390, width=30)
+            NoBtn = tk.Button(text="Salir", command=self.No)
+            NoBtn.place(x=70, y=390, width=50)
         else:
-            errores(BoolDesc, BoolHoras, BoolPeriodos, BoolClientes, BoolMinProd, BoolMaxProd, BoolTsel, BoolTmar,
-                    BoolTpago, BoolDist, BoolCajas)
+            WarningLbl = Label(text = "Varios Errores encontrados")
+            WarningLbl.place(x=10, y = 370)
+            self.master.geometry('600x395')
+            lblD = tk.Label()
+            lblH = tk.Label()
+            lblP = tk.Label()
+            lblC = tk.Label()
+            lblDist = tk.Label()
+            lblCajas = tk.Label()
+            lblMin = tk.Label()
+            lblMax = tk.Label()
+            lblTsel = tk.Label()
+            lblTmar = tk.Label()
+            lblTpago= tk.Label()
 
+            if not BoolDesc:
+                lblD['text'] = 'Error en la Descripcion ingresada.'
+            lblD.place(x=285, y=10)
+            if not BoolHoras:
+                lblH['text']="Error en las horas ingresadas, debe ser mayor que 0."
+            lblH.place(x=285,y=40)
+            if not BoolPeriodos:
+                lblP['text']="Error en numero de periodos, debe ser mayor que 0."
+            lblP.place(x=285,y=70)
+            if not BoolClientes:
+                lblC['text']="Error en la cantidad de clientes, debe ser mayor que 0."
+            lblC.place(x=285,y=100)
+            if not BoolDist:
+                lblDist['text']== "Error en la distribicion de cajas, deben ser "+ str(Periodos)+" valores y sumar 100"
+                self.master.geometry('650x375')
+            lblDist.place(x=285,y=130)
+            if not BoolCajas:
+                lblCajas['text']="Error en la cantidad de cajas, deben ser "+ str(Periodos) +" valores"
+            lblCajas.place(x=285,y=160)
+            if not BoolMinProd:
+                lblMin['text']="Error en cantidad minima de productos, debe ser mayor o igual a 0."
+            lblMin.place(x=285, y=190)
+            if not BoolMaxProd:
+                lblMax['text']="Error en cantidad maxima de productos, debe ser mayor que "+str(Minprod)+"."
+            lblMax.place(x=285,y=220)
+            if not BoolTsel:
+                lblTsel['text']="Error en tiempo promedio seleccion de productos, debe ser mayor que 0."
+            lblTsel.place(x=285,y=250)
+            if not BoolTmar:
+                lblTmar['text']="Error en tiempo promedio de marcado, debe ser mayor que 0."
+            lblTmar.place(x=285,y=280)
+            if not BoolTsel:
+                lblTpago['text']="Error en tiempo promedio de pago, debe ser mayor que 0."
+            lblTpago.place(x=285,y=310)
 
-
-
-def errores(BoolDesc, BoolHoras, BoolPeriodos, BoolClientes, BoolMinProd, BoolMaxProd, BoolTsel, BoolTmar, BoolTpago,
-            BoolDist, BoolCajas):
-    error = Tk()
-    error.title("Errores Encontrados")
-    error.geometry('300x300')
-    LblD = Label(text="")
-    LblD.place(x=20, y=10)
-
-    if not BoolDesc:
-        LblD = Label(text="Error Ingresando la descripcion")
-    if not BoolHoras:
-        LblH = Label(text="Error Ingresando la cantidad de horas")
-        LblH.pack()
-    if not BoolPeriodos:
-        LblP = Label(text="Error Ingresando la cantidad de horas")
-        LblP.pack()
-    if not BoolClientes:
-        LblCL = Label(text="Error Ingresando la cantidad de horas")
-        LblCL.pack()
-    if not BoolMinProd:
-        LblMin = Label(text="Error Ingresando la cantidad de horas")
-        LblMin.pack()
-    if not BoolMaxProd:
-        LblMax = Label(text="Error Ingresando la cantidad de horas")
-        LblMax.pack()
-    if not BoolTsel:
-        LblTsel = Label(text="Error Ingresando la cantidad de horas")
-        LblTsel.pack()
-    if not BoolTmar:
-        LblTmar = Label(text="Error Ingresando la cantidad de horas")
-        LblTmar.pack()
-    if not BoolTpago:
-        LblTpago = Label(text="Error Ingresando la cantidad de horas")
-        LblTpago.pack()
-    if not BoolDist:
-        LblDist = Label(text="Error Ingresando la cantidad de horas")
-        LblDist.pack()
-    if not BoolCajas:
-        LblCajas = Label(text="Error Ingresando la cantidad de horas")
-        LblCajas.pack()
-
-    error.mainloop()
-
-
+    def No(self):
+        self.master.destroy()
 def main():
-    root = Tk()
-    root.title = "Simulador"
-    root.geometry('500x375')
-    app = Simulador(root)
-    root.mainloop()
-
+    retornar()
 
 if __name__ == '__main__':
     main()
